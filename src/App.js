d1f6e919168b7dcaa59b697f19b43e1fe64efa3d
Mike <mike.shan1993@gmail.com>
2023-09-12 11:37:17 +0200

    Static version of the To-Do list

--- src/App.js
+++ src/App.js
@@ -8,58 +8,24 @@ function MainApp() {
   return (
     <section>
       <h1>To-do list</h1>
-      <List />
+      <ToDoList />
     </section>
   );
 }
 
-function List() {
+const ToDos= [
+  {label: "To-do Item 1", id:"tdi1", checked:'true'},
+  {label: "To-do Item 2", id:"tdi2", checked:'false'},
+  {label: "To-do Item 3", id:"tdi3", checked:'false'}
+]
 
-  const [listItems, setListItems] = useState(["To-do Item 1", "To-do Item 2", "To-do Item 3"])
-  const [item, setItem] = useState(null);
-
-  function handleClick(){
-    if(item !== '')
-      setListItems([...listItems, item])
-      document.querySelector('input[type=text]').value = '';
-  }
-  
-  return (
-    <>
-      <ul>
-        {listItems.map((el) => (
-          <ListItem label={el} />
-        ))}
-      </ul>
-      <input type="text" onChange={() => setItem(document.querySelector('input[type=text]').value)}></input>
-      <button onClick={handleClick}>Add item</button>
-  </>)
-}
-
-function ListItem({ label, addItem }) {
-  const [checked, setChecked] = useState('');
-
-  function handleClick() {
-    if (checked) {
-      setChecked(false);
-      console.log('List-item unchecked');
-    } else {
-      setChecked(true);
-      console.log('List-item checked');
-    }
-  }
-
+function ToDoListItem({ toDoItem }) {
   return (
     <li>
-      <input
-        id={label}
-        type="checkbox"
-        defaultChecked={checked}
-        onClick={handleClick}
-      ></input>
-      <label htmlFor={label}>{label}</label>
+      <input type="checkbox" id={toDoItem.id} defaultChecked={toDoItem.checked}></input>
+      <label htmlFor={toDoItem.id}>{toDoItem.label}</label>
     </li>
-  );
+  )
 }
 
 function App() {
